585eba6bea80aa1fad10fdcd72599d3711041ec4
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-03-05 15:29:48 +0100

    Commit reto buclesFor corregido

--- Retos 1/buclesFor.ts
+++ Retos 1/buclesFor.ts
@@ -5,22 +5,22 @@
 
 function evenNumbers(num:number){
 
-    let arrayNumbers: number[] = [];
-    let arrayEven: number[] = [];
+    function evenNumbers(num:number){
 
-    for(let i=0; i < num; i++){
-       arrayNumbers.push(i)       
+        let arrayNumbers: number[] = [];
+        let impar: number = 0;
+    
+        for(let i=0; i < num; i++){
+            arrayNumbers.push(i)      
+    
+            if(arrayNumbers[i] % 2 != 0){
+                impar = arrayNumbers[i] 
+                console.log(impar)  
+            }   
+        } 
     }
-    for(let i=0; i <= arrayNumbers.length; i++){
-
-        if(arrayNumbers[i] % 2 != 0){
-            arrayEven.push(i)
-        }
-    }
-    return(arrayEven)
-}
-
-console.log(evenNumbers(19));
+    
+    evenNumbers(18);
 
 // 3. Realizar una función que como parámetro de entrada reciba un array de strings y como
 // salida devuelva el array revertido. No se puede utilizar el método revert de la clase array
